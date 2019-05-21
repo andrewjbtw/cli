dac918d31d3f0950f685049b984dea7b46c700a5
Andrew Berger <andrewjberger@gmail.com>
2019-05-21 00:23:27 -0700

    Configured atom feed.

--- sitefiles/publishconf.py
+++ sitefiles/publishconf.py
@@ -13,8 +13,8 @@ from pelicanconf import *
 SITEURL = 'https://cli.suppliedtitle.org'
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+FEED_ALL_ATOM = 'feeds/posts.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
